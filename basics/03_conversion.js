768c7586c867e6fd134c8357023c4bfcdc0b3f48
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-21 04:30:03 +0000

    ConversionOfDatatypes

---
+++ basics/03_conversion.js
@@ -0,0 +1,26 @@
+// let score="rohit"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInno=Number(score);
+// console.log(valueInno);
+
+//33 => 33
+//33abc => NaN
+//true=1, false=>0
+
+let logges=1;
+let booleanlogges=Boolean(logges);
+console.log(booleanlogges);
+
+//0=>false , 1=>true
+//""=>false (empty string)
+//"rohit"=>true (string with some value)
+
+let someno=33;
+let strNo=String(someno)
+console.log(strNo);
+
+
+//              
