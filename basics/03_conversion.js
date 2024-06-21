223e1482ecf620a2fb7a2960054aebc34cb797d5
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-21 04:42:58 +0000

    Operations

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -10,17 +10,52 @@
 //33abc => NaN
 //true=1, false=>0
 
-let logges=1;
-let booleanlogges=Boolean(logges);
-console.log(booleanlogges);
+// let logges=1;
+// let booleanlogges=Boolean(logges);
+// console.log(booleanlogges);
 
 //0=>false , 1=>true
 //""=>false (empty string)
 //"rohit"=>true (string with some value)
 
-let someno=33;
-let strNo=String(someno)
-console.log(strNo);
+// let someno=33;
+// let strNo=String(someno)
+// console.log(strNo);
 
 
-//              
+// *********************** Operations ***********************
+
+// let val=3;
+// let neg=-val;
+// console.log(neg);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="rohit";
+// let str2=" maske";
+
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
