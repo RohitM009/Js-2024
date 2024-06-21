f4c0a88889ea89a5cba58e847b8ef5e8f2b1fb8c
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-21 10:14:09 +0000

    part1

---
+++ basics_02/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+
+// let arr=[1,2,3,4,5,6]
+
+// const myHeors = ["shaktiman", "naagraj"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+
+// console.log(myArr2[2]);
+
+//Array Methods
+
+// arr.push(7)
+// arr.pop()
+
+// //add at the start of the array
+// arr.unshift(9)
+// // arr.shift() //removes the first elemet 
+// console.log(arr.includes(10));
+// console.log(arr.indexOf(10));
+
+// const newArr=arr.join() //it is used to convert array into stirng 
+// console.log(typeof newArr);
+
+// console.log(arr);
+
+//slice  ,splice
+// let newArr=[1,2,3,4,5,6]
+// console.log("A",newArr)
+
+//slice it will not manipulate the original arrray
+// const arr=newArr.slice(1,3);
+// console.log(arr);            //output = (2,3)
+// console.log(`ORiginal Array${newArr}`);          //output => (1,2,3,4,5,6)
+
+
+//splice will manipulate the array 
+// const arr2=newArr.splice(1,3);
+// console.log(arr2);           //o/p=> (2,3,4) 
+// console.log(`ORiginal Array${newArr}`);      //output=> (1,5,6)
+
+
