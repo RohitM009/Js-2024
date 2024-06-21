18120d8b1924d0784eacde476291de97b4045c73
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-21 05:08:30 +0000

    datatypes

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const no=100
+const no1=100.6
+const isLogged=false
+const outside=null
+let useremail //(undefined)
+const id=Symbol('123')
+const id2=Symbol('123')
+console.log(id === id1);
+
+const bigNo=234567834567n
+
+
+
+//Reference (Non Primitive)
+// Arrays, Objects, Functions
+
+const heros=['nagaraj','shaktiman','doga']
+let myinfo=
+{
+    name:"rohit",
+    age:21,
+}
+
+const myFunction = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
