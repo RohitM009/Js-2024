48416d50aa64ab479781c8edf061ccbade7681e0
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-21 11:48:46 +0000

    jsoninfo

---
+++ basics_02/04_objects.js
@@ -0,0 +1,81 @@
+// const tinder= new Object()
+const tinderUser={}
+
+tinderUser.id="123abc"
+tinderUser.name="sam"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser);
+
+
+// const regularUser={
+//     email:"sam@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname : "rohit",
+//             lastname : "maske"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname);
+
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+// // const obj3 = { obj1, obj2 }
+// // const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// const users=[
+//     {
+//         if:1,
+//         email:"h@gmail.com"
+//     }, 
+//     {
+//         id: 1,
+//         email: "hfwfw@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "hdsfhytrefdweredw@gmail.com"
+//     },
+// ]
+
+// console.log(users[1].email)
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+// const cousrs={
+//     name: "JsAll",
+//     Fees: 999,
+//     instrutor: "hitest"
+// }
+// // console.log(cousrs.instrutor)
+
+// const {instrutor:cousrsInsturctor}=cousrs;  
+//here destructuring is done of cousrse object which is used in react when we use curly barces{}
+//got to know that thers is destcurting is done
+// console.log(cousrsInsturctor);
+
+
+//json format we got from the api
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
+
