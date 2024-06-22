4675a86b8b2863b0f786f6f39fe4a9d20a75db1f
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-22 11:11:59 +0000

    chaining

---
+++ iteration_05/seven.js
@@ -0,0 +1,12 @@
+const myno=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newn=myno.map((num)=> num+10)
+// console.log(newn);
+
+const newnums=myno
+                .map((num)=> num*10)
+                .map((num)=> num+1)
+                .filter((num)=> num>=40)
+
+
+
