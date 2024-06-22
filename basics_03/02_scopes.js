9094bc830afd36e3d866ac3736e1dd3f328a3a26
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-22 02:40:18 +0000

    local_global

---
+++ basics_03/02_scopes.js
@@ -0,0 +1,46 @@
+//var c=200
+let a=2000
+if(true){
+    let a=10
+    const b=20
+    // var c=30
+    console.log(`Inner 2{a}`);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one(){
+//     const username="rohit"
+
+//     function two(){
+//         const website="youtube"
+//         console.log(username);
+//     }
+//     // console.log(website)
+//     two()
+// }
+// // one()
+
+// if(true){
+//     const username="rohit"
+//     if(username === "rohit"){
+//         const website="youtube"
+//         console.log(username+website);
+//     }
+//     // console.log(website)
+// }
+// // console.log(username);
+
+//************************************* 
+
+function addone(val){
+    return val+1
+}
+addone(5)
+
+
+addteo(5)
+const addteo = function(num){
+    return num+2
+}
