e50287d73a4ce7e7764aca41f5a7de45a893fbb6
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-22 04:43:43 +0000

    trhthy_ternary

---
+++ control_flow_04/truthy_falsy_value.js
@@ -0,0 +1,46 @@
+// const useremail="re@gmail.com"
+
+// if(useremail){
+//     console.log("Got user email")
+// }
+// else{
+//     console.log("Don't got the user mail");
+// }
+
+//falsy values
+//these are the falsy values that are assumed 
+//false,0 ,-0,BigInt 0n,"",null, undefined,NaN
+
+//truthy values
+//
+//"0" , 'false'," ",[],{},function(){},
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const boj={}
+
+// if(Object.keys(boj).lneght===0){
+//     console.log("Object is empty");
+// }
+
+//Nullish Coalescing Operator (??): null, undefined
+
+let val1;
+// val1=5 ?? 10
+// val1=null ?? 10
+// val1=undefined ?? 20
+// val1=null ?? 10 ??50
+
+// console.log(val1);
+
+
+/****************** */
+
+//Ternary Operator
+
+//conditon ? true : false
+
+const price=100
+price>=90 ? console.log("less than 90"): console.log("more than 80");;
