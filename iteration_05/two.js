b37ad4228d0c037fd1fa91a485853efe72375ec6
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-22 06:35:33 +0000

    while

---
+++ iteration_05/two.js
@@ -0,0 +1,20 @@
+//while
+
+let i=0;
+// while(i<=20){
+//     console.log(`Value of index is ${i}`);
+//     i=i+2
+// }
+let nyarray=['flash','superman','batmen']
+
+let arr=0;
+while(arr<nyarray.length){
+    console.log(`Value is ${nyarray[arr]}`);
+    arr++;
+}
+
+
+let score=1;
+do{
+    console.log(`Score is ${score}`);
+}while(score<=10)
