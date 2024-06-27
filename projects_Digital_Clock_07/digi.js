fdcd4c9fb3bd2c1eae3c3b106435cbb7349ed7f5
Rohit Maske <145253838+RohitM009@users.noreply.github.com>
2024-06-27 02:32:23 +0000

    Digital Clock

--- projects_Digital_Clock_07/digi.js
+++ projects_Digital_Clock_07/digi.js
@@ -1,9 +1,9 @@
-const clock=document.getElementsById('clock')
+const clock=document.getElementById('clock')
 // const clock=document.querySelector('#clock')
 
 //this setInterval is used to continuously run the method which is given 
 //it is controlled by js events
 setInterval(function(){
     let date=new Date()
-    clock.innerText=date.toLocaleTimeString();
-},1000)
+    clock.innerHTML=date.toLocaleTimeString();
+},1000);
